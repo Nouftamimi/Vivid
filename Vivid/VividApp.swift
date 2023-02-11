5d0cb1ee7ed03edb106988ffb5d867d2def2a959
Roa Mohamed <70070721+RoaPro9@users.noreply.github.com>
2023-02-11 19:25:40 +0300

    fghjkl;

---
+++ Vivid/VividApp.swift
@@ -0,0 +1,20 @@
+//
+//  VividApp.swift
+//  Vivid
+//
+//  Created by Roa Moha on 20/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct VividApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
